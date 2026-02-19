c50d7c27b78cbb8f0916ffa6c3105292c8b2b24a
genocem <ahmedkacemtn@gmail.com>
2026-02-19 20:02:33 +0100

    pushed up the logic for getting the absolute path for it to be used to get the repo's name

--- internal/run/addRepoFunc.go
+++ internal/run/addRepoFunc.go
@@ -21,6 +21,8 @@ func AddRepoFunc(path, name, namespace string) {
 		log.Fatalf("path '%s' is not a git repository", path)
 	}
 
+	path, err = filepath.Abs(path)
+
 	if name == "" {
 		name = filepath.Base(path)
 	}
@@ -37,11 +39,11 @@ func AddRepoFunc(path, name, namespace string) {
 	if namespace == "" {
 		namespace = config.GetCurrentNamespace()
 	}
-	abspath, err := filepath.Abs(path)
+
 	if err != nil {
 		log.Fatalf("problem getting absolute path of path '%s'", path)
 	}
-	if err := store.AddRepo(abspath, name, namespace); err != nil {
+	if err := store.AddRepo(path, name, namespace); err != nil {
 		log.Fatalf("add repo db function got fucked %s", err)
 	}
 	log.Printf("Repository '%s' added successfully.", name)
